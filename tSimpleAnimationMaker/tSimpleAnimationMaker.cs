88e7036aa6efcc0df2bc4d38596eaf8960ec7f86
XVoltzX <itsmeomar2003@yahoo.com>
2017-01-14 19:23:17 +0200

    revamp from raydeejay

--- tSimpleAnimationMaker/tSimpleAnimationMaker.cs
+++ tSimpleAnimationMaker/tSimpleAnimationMaker.cs
@@ -1,144 +1,161 @@
-﻿using System;
+using System;
 using System.IO;
 
 namespace tSimpleAnimationMaker
 {
 	class MainClass
 	{
-		public static string[] inputStrings = new string[5];
+		// public static string standardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+		//                                                 "tSimpleAnimationMaker");
+
+		public static string standardPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory),
+														"tSimpleAnimationMaker");
+		private static string Prompt(string str)
+		{
+			Console.WriteLine(str);
+			return Convert.ToString(Console.ReadLine());
+		}
+
+		private static int PromptInt(string str)
+		{
+			Console.WriteLine(str);
+			return Convert.ToInt32(Console.ReadLine());
+		}
+
+		private static void PromptKey(string str)
+		{
+			Console.WriteLine(str);
+			Console.ReadKey();
+		}
+
+		private static void DisplayIntro()
+		{
+			PromptKey(@"Thank you for using tSimpleAnimationMaker :)
+Version 1.0.0 By Trivaxy!
+
+(Thanks to raydeejay for revamping code)
+
+PLEASE, DO NOT CHANGE THE FOLDER NAME OR THE FILE WILL NOT GET MADE!
+You can find the file generated in " + standardPath + @"
+Press any key to continue.");
+		}
+
+		private static string AskForPath(string defaultPath)
+		{
+			string path = defaultPath;
+			while (true)
+			{
+				string answer = Prompt(@"Would you like a custom place to save the file in? Or save it to
+" + defaultPath + @"?
+(yes/no)");
+				if (answer == "yes")
+				{
+					Console.WriteLine("\nPlease input directory: (Tip: you can drag the folder here and the directory will be put!");
+					path = Convert.ToString(Console.ReadLine());
+					break;
+				}
+				else if (answer == "no")
+				{
+					Console.WriteLine("You can find the file generated in " + path);
+					Console.WriteLine("Make sure you have a folder name called tSimpleAnimationMaker\nin your desktop, otherwise the program will crash.");
+					break;
+				}
+				else {
+					Console.WriteLine("Bro, put an actual answer in...");
+				}
+			}
+			return path;
+		}
+
+		private static string AskForAnimationType()
+		{
+			string answer = "";
+			while (true)
+			{
+				answer = Prompt("\nWhat kind of animation is this? (npc/projectile)");
+				if (answer == "npc" || answer == "projectile")
+				{
+					break;
+				}
+				else {
+					Console.WriteLine("Bro, put an actual answer in...");
+				}
+			}
+			return answer;
+		}
+
+		private static void WriteNPCToFile(string filename, int frames, int ticksPerFrame)
+		{
+			string output = @"// Please put: Main.npcFrameCount[npc.type] = " + frames + @";   In SetDefaults!
+    public override void FindFrame(int frameHeight) {
+        npc.frameCounter++;
+        if (npc.frameCounter >= " + ticksPerFrame + @") {
+            npc.frame.Y += frameHeight;
+              
+            if (npc.frame.Y >= Main.npcFrameCount[npc.type] * frameHeight) {
+                npc.frame.Y = 0;
+            }
+            
+            npc.frameCounter = 0;
+        }
+    }";
+			using (StreamWriter file = new StreamWriter(filename, false))
+			{
+				file.WriteLine(output);
+			}
+		}
+
+		private static void WriteProjectileToFile(string filename, int frames, int ticksPerFrame)
+		{
+			string output = @"// Please put: Main.projFrames[projectile.type] = " + frames + @";   In SetDefaults!
+    public override bool PreDraw(SpriteBatch sb, Color lightColor) {
+        projectile.frameCounter++;
+        if (projectile.frameCounter >= " + ticksPerFrame + @") {
+            projectile.frame++;
+            projectile.frameCounter = 0;
+
+            if (projectile.frame > " + frames + @") {
+                projectile.frame = 0;  
+            }
+        } 
+        return true;
+    }";
+			using (StreamWriter file = new StreamWriter(filename, false))
+			{
+				file.WriteLine(output);
+			}
+		}
 
 		public static void Main(string[] args)
 		{
-				// Delete the file if it exists.
-			Console.WriteLine("Thank you for using tSimpleAnimationMaker :)");
-			Console.WriteLine("Version 1.0.0 By Trivaxy!");
-			Console.WriteLine("Press any key to continue.");
- 			Console.ReadKey();
-			Console.WriteLine("\nPlease input directory: (Tip: you can drag the folder here and the directory will be put!");
-			inputStrings[3] = Convert.ToString(Console.ReadLine());
-			Console.WriteLine("\nIs this an NPC animation? (yes/no)");
+			DisplayIntro();
 
-			inputStrings[0] = Convert.ToString(Console.ReadLine());
+			string path = AskForPath(standardPath);
+			string animationType = AskForAnimationType();
 
-			if (inputStrings[0] != "yes" && inputStrings[0] != "no")
+			int frames = PromptInt(@"Okay, I just need you to tell me 2 things.
+How many frames does your animation have?");
+
+			int ticksPerFrame = PromptInt(@"Okay, how fast do you want the animation to be?
+(60 = 1 frame per second, 30 = 1 frame per half a second.. etc.
+ The lower the number, the faster it is)");
+
+			Console.WriteLine("Making file...");
+
+			Directory.CreateDirectory(path);
+
+			if (animationType == "npc")
 			{
-				Console.WriteLine("Oh wow, you didn't type in yes or no.. Well, restart the program.");
+				WriteNPCToFile(Path.Combine(path, "NPCAnimation.cs"), frames, ticksPerFrame);
 			}
-			else if (inputStrings[0] == "yes")
+			else if (animationType == "projectile")
 			{
-				int frameInput;
-				int speedInput;
-
-				Console.WriteLine("\nNPC Animation, huh? Okay, I just need you to tell me 2 things.");
-				Console.WriteLine("How many frames does your NPC have?");
-
-				frameInput = Convert.ToInt32(Console.ReadLine());
-
-				Console.WriteLine("Okay, how fast do you want the animation to be?");
-				Console.WriteLine("(60 = 1 frame per second, 30 = 1 frame per half a second.. etc. \nThe lower the number the faster it is)");
-
-				speedInput = Convert.ToInt32(Console.ReadLine());
-
-				Console.WriteLine("Making file...");
-
-					using (var myFile = File.Create(Path.Combine(inputStrings[3], "Animation.cs")))
-					{
-						//WOOT WOOT NOTHING HAPPENS HERE! :D I hope you find my simple and ugly code useful though :)
-					}
-
-					using (StreamWriter file =
-						   new StreamWriter(Path.Combine(inputStrings[3], "Animation.cs"), true))
-					{
-						file.WriteLine("//Please put: Main.npcFrameCount[npc.type] = " + frameInput + ";   In SetDefaults!");
-						file.WriteLine("public override void FindFrame(int frameHeight)");
-						file.WriteLine("{");
-						file.WriteLine(" npc.frameCounter++;");
-						file.WriteLine("   if(npc.frameCounter >= " + speedInput + ")");
-						file.WriteLine("   {");
-						file.WriteLine("    npc.frame.Y += frameHeight;");
-						file.WriteLine();
-						file.WriteLine("       if(npc.frame.Y >= Main.npcFrameCount[npc.type] * frameHeight");
-						file.WriteLine("        npc.frame.Y = 0;");
-						file.WriteLine("        npc.frameCounter = 0;");
-						file.WriteLine("   }");
-						file.WriteLine("}");
-					}
-
-					Console.WriteLine("File generated at: " + inputStrings[3]);
-					Console.WriteLine("Finished! Thank you for using tSimpleAnimationMaker.");
-					Console.ReadKey();
-			}
-			if (inputStrings[0] == "no")
-			{
-				Console.WriteLine("\nIs it a projectile animation? (yes/no)");
-				inputStrings[1] = Convert.ToString(Console.ReadLine());
+				WriteProjectileToFile(Path.Combine(path, "ProjectileAnimation.cs"), frames, ticksPerFrame);
 			}
 
-			if (inputStrings[1] == "no")
-			{
-				Console.WriteLine("Then what is it?... Meh, restart the program!");
-				Console.ReadKey();
-			}
-
-			else if (inputStrings[1] == "yes")
-			{
-				int inputframes;
-				int inputspeed;
-
-				Console.WriteLine("\nHow many frames does your projectile have?");
-
-				inputframes = Convert.ToInt32(Console.ReadLine()) - 1;
-
-				Console.WriteLine("\nOkay, how fast do you want the animation to be?");
-				Console.WriteLine("(60 = 1 frame per second, 30 = 1 frame per half a second.. etc. \nThe lower the number the faster it is)");
-
-				inputspeed = Convert.ToInt32(Console.ReadLine());
-
-				Console.WriteLine("Making file...");
-
-					using (var myFile = File.Create(Path.Combine(inputStrings[3], "Animation.cs")))
-					{
-						//..end my life please.
-					}
-
-					             string outputtop =
-					
-	@"public override bool PreDraw(SpriteBatch sb, Color lightColor)
-	{
-	  projectile.frameCounter++;";
-					             string outputmiddle =
-					
-@"	  {
-		projectile.frame++;
-		projectile.frameCounter = 0;";
-
-					             string outputbottom =
-					
-@"	  	 projectile.frame = 0;
-					}
-
-	     return true;
-	}";
-
-					using (StreamWriter file =
-					       new StreamWriter(Path.Combine(inputStrings[3], "Animation.cs"), true))
-					{
-						file.WriteLine("//Please put: Main.projFrames[projectile.type] = " + inputframes + ";   In SetDefaults!");
-						file.WriteLine(outputtop);
-						file.WriteLine("\tif (projectile.frameCounter >= " + inputspeed + ")");
-						file.WriteLine(outputmiddle);
-						file.WriteLine("\n   if (projectile.frame > " + inputframes + ")");
-						file.WriteLine(outputbottom);
-					}
-
-					Console.WriteLine("File generated at: " + inputStrings[3]);
-					Console.WriteLine("Finished! Thank you for using tSimpleAnimationMaker.");
-					Console.ReadKey();
-           }
-			if (inputStrings[1] != "yes" && inputStrings[1] != "no")
-			{
-				Console.WriteLine("Bro, put an actual answer in...");
-			}
+			PromptKey(@"File generated at: " + path + @"
+Finished! Thank you for using tSimpleAnimationMaker.
+Press any key to exit.");
 		}
 	}
 }
