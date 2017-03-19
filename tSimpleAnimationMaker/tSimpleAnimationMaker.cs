70743dd0c3e2dfafd9e17d56253b4ebd1fb3b201
Trivaxy <itsmeomar2003@yahoo.com>
2017-03-19 09:04:06 +0200

    Update tSimpleAnimationMaker.cs

--- tSimpleAnimationMaker/tSimpleAnimationMaker.cs
+++ tSimpleAnimationMaker/tSimpleAnimationMaker.cs
@@ -64,7 +64,7 @@ Press any key to continue.");
 					Console.WriteLine("Bro, put an actual answer in...");
 				}
 			}
-			path = path.Replace("/"", "");
+			path = path.Replace("\"", "");
 			return path;
 		}
 
